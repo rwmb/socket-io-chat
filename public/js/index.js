99fb3c843fd2d6d316e1e30617802d39d9a207c6
rwmb <richard.wmbv@gmail.com>
2017-11-01 11:25:20 +0000

    add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,15 @@
 
 var socket = io();
+var format = function (message) {
+  let date = new Date(message.createdAt);
+  dateString = date.getDate() + 
+    '/' + date.getMonth() + 
+    '/' + date.getFullYear() +
+    ' ' + date.getHours() +
+    ':' + date.getMinutes();
+  return `${dateString} - ${message.from}: ${message.text}`;
+};
+
 socket.on('connect', function () {
   console.log('Connected to server');
 
@@ -10,13 +20,19 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
-  let date = new Date(message.createdAt);
-  dateString = date.getDate() + 
-    '/' + date.getMonth() + 
-    '/' + date.getFullYear() +
-    ' ' + date.getHours() +
-    ':' + date.getMinutes()
-  console.log(dateString + ' - ' + 
-    message.from + ': ' + 
-    message.text);
+  var li = $('<li></li>');
+  li.text(format(message));
+  
+  $('#messages').append(li);
+});
+
+$('#message-form').on('submit', function (e) {
+  e.preventDefault();
+  
+  socket.emit('createMessage', {
+    from: 'User',
+    text: $('[name=message]').val()
+  }, function () {
+    
+  });
 });
--- server/server.js
+++ server/server.js
@@ -18,11 +18,12 @@ io.on('connection', (socket) => {
   socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app!'));
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined.'));
   
-  socket.on('createMessage', (newMessage) => {
+  socket.on('createMessage', (newMessage, callback) => {
     console.log(`${newMessage.from}: ${newMessage.text}`); // log message
     
     //socket.broadcast.emit('newMessage', newMessage); // send to everyone but who sent the message
     io.emit('newMessage', generateMessage(newMessage.from, newMessage.text)); // send to everyone
+    callback('This is from the server.');
   });
   
   socket.on('disconnect', () => {
